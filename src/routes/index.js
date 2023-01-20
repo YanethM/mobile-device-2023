2d7553b40ec7a100fea8003c959e041933650438
YanethM <yaneth.mejiar@autonoma.edu.co>
2023-01-19 20:21:12 -0500

    Test the POST request

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,8 +3,6 @@ const USER_ROUTES = require("./user");
 
 const ROUTES_APP = (APP) => {
   const ROUTES = EXPRESS.Router();
-  /* POST: http://localhost:3000/api/v1/customers/user */
-  /* GET: http://localhost:3000/api/v1/customers/ */
   APP.use("/api/v1", ROUTES);
   ROUTES.use("/customers", USER_ROUTES);
 };
